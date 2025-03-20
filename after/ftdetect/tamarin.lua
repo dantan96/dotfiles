d70d65ef940d6d4ddee9fcda1a15c9ecf8afd6f1
dantan96 <danieltanios@gmail.com>
2025-03-20 16:55:08 +1100

    Update after/ftdetect/tamarin.lua to use spthy filetype for consistent syntax handling

--- after/ftdetect/tamarin.lua
+++ after/ftdetect/tamarin.lua
@@ -8,14 +8,14 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   group = augroup,
   pattern = { "*.spthy", "*.sapic" },
   callback = function()
-    -- Set the filetype to tamarin
-    vim.bo.filetype = "tamarin"
+    -- Set the filetype to spthy
+    vim.bo.filetype = "spthy"
     
     -- Log the detection if debug mode is enabled
     if vim.fn.isdirectory('/tmp/tamarin-debug') == 1 then
       local log_file = io.open("/tmp/tamarin-debug/ftdetect.log", "a")
       if log_file then
-        log_file:write(os.date("%Y-%m-%d %H:%M:%S") .. " Detected tamarin filetype for: " .. vim.fn.expand("%:p") .. "\n")
+        log_file:write(os.date("%Y-%m-%d %H:%M:%S") .. " Detected spthy filetype for: " .. vim.fn.expand("%:p") .. "\n")
         log_file:close()
       end
     end
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 
 -- For backwards compatibility with older Neovim versions, add the ftdetect command
 vim.cmd([[
-  au BufRead,BufNewFile *.spthy setfiletype tamarin
-  au BufRead,BufNewFile *.sapic setfiletype tamarin
+  au BufRead,BufNewFile *.spthy setfiletype spthy
+  au BufRead,BufNewFile *.sapic setfiletype spthy
 ]])
 
